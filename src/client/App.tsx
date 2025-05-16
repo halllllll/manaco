fd094ada0977261a95fe8703e187fcdc2696ff08
h4lllllll <hjod1172@yahoo.co.jp>
2025-05-16 01:58:58 +0000

    import daisyui

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,6 @@ import { GASClient } from 'gas-client';
 import { isGASEnvironment } from 'gas-client/src/utils/is-gas-environment';
 import { type FC, useEffect, useState } from 'react';
 import type * as server from '../server/main';
-import './App.css';
 import { SheetNameAPI, SheetUrlAPI } from './stubs/getSheetInfo';
 
 const { serverFunctions } = new GASClient<typeof server>();
@@ -28,12 +27,13 @@ const App: FC = () => {
   }, []);
 
   return (
-    <>
-      <div className="container-mx-auto p-4 ">
+    <div className="flex flex-col min-h-screen">
+      <div className="container mx-auto p-4 bg-gray">
         <a href="/src/server/Menu/menu.html">（カスタムメニューのhtmlサンプル）</a>
         <h1>{title !== '' ? title : 'Vite + React on GAS'}</h1>
         <div className="card">
           <button
+            className="btn btn-primary"
             type={'button'}
             onClick={() => {
               setCount((count: number) => count + 1);
@@ -42,14 +42,16 @@ const App: FC = () => {
             count is {count}
           </button>
           <div className="card">
-            <button
-              type={'button'}
-              onClick={async () => {
-                await handleButtonClick();
-              }}
-            >
-              SpreadSheetにカウントを反映する
-            </button>
+            <div className="card-body">
+              <button
+                type={'button'}
+                onClick={async () => {
+                  await handleButtonClick();
+                }}
+              >
+                SpreadSheetにカウントを反映する
+              </button>
+            </div>
           </div>
         </div>
         {isGASEnvironment() ? (
@@ -66,10 +68,12 @@ const App: FC = () => {
           <div>here is DEV env</div>
         )}
       </div>
-      <footer className="fotter">
-        <p>v2024-07-11</p>
-      </footer>
-    </>
+      <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content mt-auto p-4">
+        <aside>
+          <p>Copyright © {new Date().getFullYear()} - GIG SCHOOL</p>
+        </aside>
+      </footer>{' '}
+    </div>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite';
 // import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -5,7 +6,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), viteSingleFile()],
+  plugins: [react(), tsconfigPaths(), viteSingleFile(), tailwindcss()],
   build: {
     outDir: 'dist',
   },
