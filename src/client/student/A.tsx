38c1e82246cc860726c99de87c8b3db550976b38
h4lllllll <hjod1172@yahoo.co.jp>
2025-06-02 13:19:09 +0000

    a

---
+++ src/client/student/A.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+export const Aaa: FC = () => {
+  return (
+    <>
+      <h1>AAA</h1>
+      <p>これはAAAコンポーネントです。</p>
+      {/** 適当なfetchを用意し、mswの動く最小環境を検証する */}
+    </>
+  );
+};
