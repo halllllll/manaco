268de37f37e01e60758529828856108c3a9b8f67
h4lllllll <hjod1172@yahoo.co.jp>
2025-06-24 05:48:05 +0000

    fix

--- src/client/api/settings/hook.ts
+++ src/client/api/settings/hook.ts
@@ -4,9 +4,5 @@ import { SettingsAPI } from './settings';
 
 export const useSettings = () => {
   const { data, error, isLoading, mutate } = useSWR(API_ENDPOINTS.SETTINGS, SettingsAPI.get);
-  if (!isLoading) {
-    console.info('出たよsetting data');
-    console.log(data);
-  }
   return { data, error, isLoading, mutate };
 };
